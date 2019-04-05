78867548ec21a7861a08ba25d92ef6a2fd1f598f
Bru <annabru.abh@gmail.com>
2019-04-05 13:08:51 +0200

    Added 3 components: Header, InputForm and WeatherDisplay, imported them all in App.js

---
+++ src/InputForm.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class InputForm extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default InputForm
