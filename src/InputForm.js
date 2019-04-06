416f292cb5a0a18b76a08a671b6e712e95970169
Bru <annabru.abh@gmail.com>
2019-04-07 00:03:12 +0200

    Failed to fetch - will try again tomorrow.

--- src/InputForm.js
+++ src/InputForm.js
@@ -3,15 +3,15 @@ import WeatherDisplay from './WeatherDisplay'
 import './assets/styles/InputForm.css'
 
 export class InputForm extends Component {
-  render() {
-    return (
-      <div>
-        <h3>Which city's weather do you want to check?</h3>
-        <textarea rows="3" cols="30" placeholder="Write your wanted location here" className="inputForm"></textarea>
-        <WeatherDisplay />
-      </div>
-    )
+    render() {
+      return (
+        <div>
+          <h3>Which city's weather do you want to check?</h3>
+          <textarea rows="3" cols="30" placeholder="Write your wanted location here" className="inputForm"></textarea>
+          <WeatherDisplay />
+        </div>
+      )
+    }
   }
-}
-
+  
 export default InputForm
