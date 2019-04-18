6c809a37cc1590b67c6b15f627d778bd64c06650
Bru <annabru.abh@gmail.com>
2019-04-18 18:27:53 +0200

    WEATHER DISPLAY WORRRRRRRRRKSSSSSSSSSSSSSSSSSSSS YASSSSSSSS

--- src/components/WeatherDisplay.js
+++ src/components/WeatherDisplay.js
@@ -3,7 +3,13 @@ import "../assets/styles/WeatherDisplay.css";
 
 export class WeatherDisplay extends Component {
   render() {
-    return <div>{this.props.temperature}</div>;
+    return <React.Fragment>
+      <h3>City: {this.props.city}</h3>
+      <h3>Country: {this.props.country}</h3>
+      <h3>Weather: {this.props.description} </h3>
+      <h3>Temperature (in Celcius): {this.props.temperature}</h3>
+      <h3>Humidity: {this.props.humidity}</h3>    
+    </React.Fragment>;
   }
 }
 
