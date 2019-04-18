521d7d344760c36234a1259a375d0155bdb3f796
Bru <annabru.abh@gmail.com>
2019-04-18 15:11:05 +0200

    Creating getWeather method in App.js to do the api call and see if it works this time

--- src/App.js
+++ src/App.js
@@ -6,9 +6,13 @@ import InputForm from './components/InputForm'
 import './assets/styles/App.css';
 
 const API_KEY = "9ed85315e283641973f2df53aa78b4ad
-"
-
 class App extends Component {
+  //this is a method
+  getWeather = () => {
+
+  }
+
+
   render() {
     return (
       <div className="App">
