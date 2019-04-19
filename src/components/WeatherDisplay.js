3febce8d1a254017aef84c1a68827d3979027a6c
Bru <annabru.abh@gmail.com>
2019-04-19 19:29:59 +0200

    using conditional operators in weatherdisplay.js so the "titles" for the weather details appear only when clicking the button and are not hanging there with empty values the whole time

--- src/components/WeatherDisplay.js
+++ src/components/WeatherDisplay.js
@@ -3,13 +3,21 @@ import "../assets/styles/WeatherDisplay.css";
 
 export class WeatherDisplay extends Component {
   render() {
-    return <React.Fragment>
-      <h3>Location: {this.props.city}, {this.props.country}</h3>
-  
-      <h3>Weather: {this.props.description} </h3>
-      <h3>Temperature (in Celcius): {this.props.temperature}</h3>
-      <h3>Humidity: {this.props.humidity}</h3>    
-    </React.Fragment>;
+    return (
+      <React.Fragment>
+        {/* conditional operator -> when both conditions meet, it'll render the whole JSX ONLY when button is pressed */}
+        {this.props.city && this.props.country && (
+          <h3>
+            Location: {this.props.city}, {this.props.country}
+          </h3>
+        )}
+        { this.props.description && (<h3>Conditions: {this.props.description} </h3>) }
+
+        
+        <h3>Temperature (in Celcius): {this.props.temperature}</h3>
+        <h3>Humidity: {this.props.humidity}</h3>
+      </React.Fragment>
+    );
   }
 }
 
