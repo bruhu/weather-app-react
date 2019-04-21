73c40c8ed4843ee4e58c3218e55d4bd93b4295af
Bru <annabru.abh@gmail.com>
2019-04-21 16:48:49 +0200

    Updates

---
+++ precache-manifest.da777d423546fdad3f2111f94878eddd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "734e7f31b5ff715f8872",
+    "url": "/weather-app-react/static/css/main.2cefd0e2.chunk.css"
+  },
+  {
+    "revision": "734e7f31b5ff715f8872",
+    "url": "/weather-app-react/static/js/main.2b42e8ca.chunk.js"
+  },
+  {
+    "revision": "b246a4fc453f9ee8e4e8",
+    "url": "/weather-app-react/static/js/runtime~main.a31b442a.js"
+  },
+  {
+    "revision": "8995ed65ce443e708012",
+    "url": "/weather-app-react/static/js/2.f6b90c5d.chunk.js"
+  },
+  {
+    "revision": "501b4f8075ab912c0e598f6a70e94b6b",
+    "url": "/weather-app-react/index.html"
+  }
+];
